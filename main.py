80c5dab500e4b986c749330bc98085cf710adc7f
Homel Kanstantsin <homelkv@gmail.com>
2024-10-24 14:33:24 +0300

    start blog with rest and DB

---
+++ main.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/', methods=["GET"])
+def get_all_posts():
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
+
+@app.route('/post/<int:post_id>', methods=["GET"])
+def show_post(post_id):
+    requested_post = BlogPost.query.filter(BlogPost.id == post_id).one_or_none()
+    return render_template("post.html", post=requested_post)
+
+
+# TODO: add_new_post() to create a new blog post
+@app.route('/new-post')
+def add_new_post():
+    return  render_template("make-post.html")
+
+
+# TODO: edit_post() to change an existing blog post
+
+# TODO: delete_post() to remove a blog post from the database
+
+# Below is the code from previous lessons. No changes needed.
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
--- real_python_course/sand_box.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timezone, timedelta
-
-
-time = datetime.now(timezone(timedelta(hours=+3)))
-print(time)
-
-
