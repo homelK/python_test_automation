25415498a70a15dfc76efa79970b03d4f988a40d
Homel Kanstantsin <homelkv@gmail.com>
2024-10-29 17:18:34 +0300

    flask_login

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 import os.path
-
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, Boolean
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
+
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+
 # CREATE TABLE IN DB
-class User(db.Model):
+class User(db.Model, UserMixin):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(1000))
 
+    def get_id(self):
+        print("INSIDE GET ID")
+        print(type(self))
+        return self
 
 with app.app_context():
     db.create_all()
 
+# create LoginManager Class
+login_manager = LoginManager()
+
+# app configuration with Login
+login_manager.init_app(app)
 
 @app.route('/')
 def home():
@@ -39,24 +50,34 @@ def home():
 def register():
     if request.method == "POST":
         data = request.form
-        new_user = User(email=data.get("email"), password=data.get("password"), name=data.get("name"))
+        hashed_password = generate_password_hash(data.get("password"), "pbkdf2:sha256", 8)
+        new_user = User(email=data.get("email"), password=hashed_password, name=data.get("name"))
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('secrets'))
+        return redirect(url_for('login'))
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        data = request.form
+        user = User.query.filter(User.email == data.get("email")).one_or_none()
+        check_passw = check_password_hash(user.password, data.get("password"))
+        if check_passw:
+            print("INSIDE LOGIN")
+            login_user(user)
+            return redirect(url_for("secrets"))
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    users = User.query.all()
-    last_user = users[len(users) - 1]
-    name = last_user.name
-    return render_template("secrets.html", user_name=name)
+    # users = User.query.all()
+    # last_user = users[len(users) - 1]
+    # name = last_user.name
+    return render_template("secrets.html")
 
 
 @app.route('/logout')
@@ -65,9 +86,16 @@ def logout():
 
 
 @app.route('/download')
+@login_required
 def download():
     dir = os.path.relpath(os.path.dirname("static/files/"))
     return send_from_directory(dir, "cheat_sheet.pdf")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    print("User_ID: ", user_id)
+    return User.get_id(user_id)
 
 
 if __name__ == "__main__":
--- sand.py
+++ sand.py
@@ -1,4 +1,12 @@
 import os
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 dir = os.path.relpath(os.path.dirname("static/files/"))
 print(dir)
+
+passw_hash = generate_password_hash("123", "pbkdf2", 8)
+check_passw = check_password_hash("pbkdf2:sha256:600000$UHPj66oh$cc3dc47a36495252a0ad94d03f1760d8e834385dcafad1be5211b1305010a98a", "123")
+
+if check_passw:
+    print("Match")
