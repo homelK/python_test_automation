8afd924be5e25ef60a0830b754b0c88de2980920
Homel Kanstantsin <homelkv@gmail.com>
2024-10-30 15:46:29 +0300

    add login check with flask

--- main.py
+++ main.py
@@ -20,17 +20,13 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
-# CREATE TABLE IN DB
+# CREATE TABLE IN DB, Add UserMixin from Flask-Login
 class User(db.Model, UserMixin):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(1000))
 
-    def get_id(self):
-        print("INSIDE GET ID")
-        print(type(self))
-        return self
 
 with app.app_context():
     db.create_all()
@@ -40,6 +36,11 @@ login_manager = LoginManager()
 
 # app configuration with Login
 login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == user_id).one_or_none()
+
 
 @app.route('/')
 def home():
@@ -54,7 +55,8 @@ def register():
         new_user = User(email=data.get("email"), password=hashed_password, name=data.get("name"))
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('login'))
+        load_user(new_user)
+        return redirect(url_for('secrets'))
     return render_template("register.html")
 
 
@@ -63,26 +65,28 @@ def login():
     if request.method == "POST":
         data = request.form
         user = User.query.filter(User.email == data.get("email")).one_or_none()
-        check_passw = check_password_hash(user.password, data.get("password"))
-        if check_passw:
-            print("INSIDE LOGIN")
+        if user is None:
+            flash("There is no user with such email")
+            return redirect(url_for("login"))
+
+        if check_password_hash(user.password, data.get("password")):
             login_user(user)
             return redirect(url_for("secrets"))
+
     return render_template("login.html")
 
 
 @app.route('/secrets')
 @login_required
 def secrets():
-    # users = User.query.all()
-    # last_user = users[len(users) - 1]
-    # name = last_user.name
-    return render_template("secrets.html")
+    return render_template("secrets.html", user=current_user)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
@@ -90,12 +94,6 @@ def logout():
 def download():
     dir = os.path.relpath(os.path.dirname("static/files/"))
     return send_from_directory(dir, "cheat_sheet.pdf")
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    print("User_ID: ", user_id)
-    return User.get_id(user_id)
 
 
 if __name__ == "__main__":
