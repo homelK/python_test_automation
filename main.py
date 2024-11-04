708f023c521dd2351a4925201374c2c6271e5218
Homel Kanstantsin <homelkv@gmail.com>
2024-11-04 17:06:22 +0300

    add comment's table to DB

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ class BlogPost(db.Model):
     author = relationship("User", back_populates="posts")
     author_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    comments = relationship("Comment", back_populates="post")
 
 
 # Create a User table for all your registered users.
@@ -66,6 +67,18 @@ class User(db.Model, UserMixin):
     password: Mapped[str] = mapped_column(String(20), nullable=False)
     name: Mapped[str] = mapped_column(String(20), nullable=False)
     posts = relationship("BlogPost", back_populates="author")
+    comments = relationship("Comment", back_populates="comment_author")
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+    comment_author = relationship("User", back_populates="comments")
+    author_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    post_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("blog_posts.id"))
+    post = relationship("BlogPost", back_populates="comments")
+
 
 
 with app.app_context():
